4cd5581d1a9263de0168db53086c372cd4a03574
Radha1403 <msradha93@gmail.com>
2020-03-06 13:04:15 +0530

    files deleted

--- .history/app_20200304112728.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-app.use((req, res, next) => {
-  console.log("In the middeleware");
-});
-const server = http.createServer(app);
-
-server.listen(3000);
