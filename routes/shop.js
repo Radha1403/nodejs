58ebd07d62e99aa3579a9401df2a5ad53deb4aca
Radha1403 <msradha93@gmail.com>
2020-03-06 15:21:31 +0530

    routing added

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 router.get("/", (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 module.exports = router;
