00b2db44c094174d6476f2a6a157fe2af6807b43
Radha1403 <msradha93@gmail.com>
2020-03-09 15:15:32 +0530

    pug changes added

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,10 @@ const path = require("path");
 const rootDir = require("../util/path");
 const products = [];
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product"
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const rootDir = require("../util/path");
 const adminData = require("./admin");
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop", path: "/" });
 });
 module.exports = router;
