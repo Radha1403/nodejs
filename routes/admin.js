bb82ca2e8aace6ae318478f8b98ecad88620ba47
Radha1403 <msradha93@gmail.com>
2020-03-10 17:51:17 +0530

    MVC pattern added

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const rootDir = require("../util/path");
-const products = [];
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    docTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+const productsController = require("../controllers/products");
 
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-exports.routes = router;
-exports.products = products;
+router.get("/add-product", productsController.getAddProduct);
+
+router.post("/add-product", productsController.postAddProduct);
+module.exports = router;
